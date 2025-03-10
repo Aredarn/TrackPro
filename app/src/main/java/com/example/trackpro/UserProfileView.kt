b449480e68566f361ed2ab441c644dc09a8f208c
Martin M. <65924165+Aredarn@users.noreply.github.com>
2025-03-10 11:32:41 +0100

    Updated userprofile. made bone structure

--- app/src/main/java/com/example/trackpro/UserProfileView.kt
+++ app/src/main/java/com/example/trackpro/UserProfileView.kt
@@ -1,2 +1,18 @@
 package com.example.trackpro
 
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
+
+
+class UserProfileView : ComponentActivity()
+{
+
+
+}
+
+
+@Composable
+fun UserProfileViewScreen()
+{
+
+}
